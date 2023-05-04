5757d3fbdf2b7dbd52349a853a64a29a46f0b147
Otácio Barbosa <otacio.barbosa@outlook.com>
2023-05-04 06:49:08 -0300

    Create meu-primeiro.component.ts

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'meu-primeiro-component',
+  template: `
+    <p>Meu primeiro compoent com Angular 2! </p>
+  `
+})
+
+export class MeuPrimeiroComponent {
+
+}
